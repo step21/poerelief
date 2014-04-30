fd15f73c33867a5a06e5c408f819b42d6729fa1c
FI <step21@devtal.de>
2014-04-30 21:01:53 +0200

    test getting xml
    gets a fixed record and dumps it to stdout

--- epidat_parse.py
+++ epidat_parse.py
@@ -1,14 +1,27 @@
 from lxml import etree
+from io import StringIO, BytesIO
+from lxml import objectify
 # Fixed baseurl for SI epidat records
-baseurl = ""
+baseurl = "http://steinheim-institut.de/cgi-bin/epidat?"
 # The seperator
 s = "-"
 # specifies the format
-format = "t"
+format = "teip5"
 # afaik records always start at 1
 id = 1
 # Fixed location for testing
 loc = "aha"
-url = baseurl + loc + s + id + s + format
+url = baseurl + "id=" + loc + s + str(id) + s + format
 
-parser = new_parser
+print url
+tree = objectify.parse(url)
+#tree = etree.parse(url)
+print tree
+root = tree.getroot()
+print root
+print(isinstance(tree.getroot(), objectify.ObjectifiedElement))
+print(objectify.dump(root))
+#xample
+#    http://steinheim-institut.de/cgi-bin/epidat?id=hha-3361
+#
+#list of epidat files
