fd35acd63e922a450324c09fba3fcf1780929ead
step21 <step21@devtal.de>
2014-06-02 21:52:37 +0200

    changes

--- epidat_parse.py
+++ epidat_parse.py
@@ -1,6 +1,7 @@
 from lxml import etree
 from io import StringIO, BytesIO
-from lxml import objectify
+#from lxml import objectify
+
 # Fixed baseurl for SI epidat records
 baseurl = "http://steinheim-institut.de/cgi-bin/epidat?"
 # The seperator
@@ -10,17 +11,32 @@ format = "teip5"
 # afaik records always start at 1
 id = 1
 # Fixed location for testing
-loc = "aha"
-url = baseurl + "id=" + loc + s + str(id) + s + format
+loc = "aha"#
+#url = baseurl + "id=" + loc + s + str(id) + s + format
 
-print url
-tree = objectify.parse(url)
+#make this a class ...
+
+
+define get_record(id, loc):
+    url = baseurl + "id=" + loc + s + id + s + format
+    tree = etree.parse(url)
+    record = tree
+
+return record
+
+define get_fields(record):
+  root = record.getroot()
+  root.find("desc")
+
+
+#print url
+#tree = objectify.parse(url)
 #tree = etree.parse(url)
-print tree
-root = tree.getroot()
-print root
-print(isinstance(tree.getroot(), objectify.ObjectifiedElement))
-print(objectify.dump(root))
+#print tree
+#root = tree.getroot()
+#print root
+#print(isinstance(tree.getroot(), objectify.ObjectifiedElement))
+#print(objectify.dump(root))
 #xample
 #    http://steinheim-institut.de/cgi-bin/epidat?id=hha-3361
 #
