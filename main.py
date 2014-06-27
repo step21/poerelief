aaff4e50736eb67144fcd7047cef6897a12d8a9d
step21 <step21@devtal.de>
2014-06-28 00:26:54 +0200

    extended main py, hebrew test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# encoding=utf-8
 #!/usr/bin/env python
+
 from flask import Flask
 from flask import render_template
 
@@ -7,7 +9,8 @@ app = Flask(__name__)
 
 #FIXME: create page class? and then subclass and / or blueprint etc...
 #how to integrate views?
-sitename = "Poetic Relief"
+sitenamed = "Poetic Relief"
+sitenameh = u"הקלה פיוטית"
 pagetitle = "Poetic dev"
 version = "0.0.1"
 
@@ -15,8 +18,8 @@ version = "0.0.1"
 @app.route('/')
 def page():
 	pg = ""
-	return render_template("index.html", pg=pg, sitename=sitename, pagetitle=pagetitle, version=version)
-mode
+	return render_template("index.html", pg=pg, sitename=sitenamed, sitenameh=sitenameh, pagetitle=pagetitle, version=version)
+
 @app.route('/demo')
 def demo():
 	ret = 'var jsonLikeString = "name:red, type:blue, multiples:green, cat:brown"'
