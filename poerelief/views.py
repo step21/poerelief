b937eafddba3a9f24f53303b24ce9a729f83e0bc
step21 <step21@devtal.de>
2014-06-30 00:11:18 +0200

    first step towards real views

--- poerelief/views.py
+++ poerelief/views.py
@@ -6,3 +6,16 @@ from poerelief import app
 @app.route('/index')
 def  index():
   return "Hello World!"
+
+@app.route('/doc/<location>/<int:docid>')
+def page(location, id):
+  pass
+
+#implement doc call giving record as json
+
+#javascript to switch texts
+
+#implement random json array?
+@app.route('/doc/random')
+def random():
+  pass
