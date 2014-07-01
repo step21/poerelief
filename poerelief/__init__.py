525725d721f9f54e3bd835eaa0e866f3bf77e294
FI <step21@devtal.de>
2014-07-01 18:28:07 +0200

    commentedout superfluous config

--- poerelief/__init__.py
+++ poerelief/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
