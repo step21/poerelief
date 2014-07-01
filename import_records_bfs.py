f0de86b4fff6872ead496d9881fb955c6690c876
FI <step21@devtal.de>
2014-07-01 18:15:48 +0200

    bfs parsing code

---
+++ import_records_bfs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding=utf-8
+import sys
+from poerelief import db, models, epidat_parse
+import untangle
+from xml.sax._exceptions import SAXParseException
+
+for x in sys.argv:
+  program_name = sys.argv[0]
+  if len(sys.argv) > 1:
+    locurl = sys.argv[1]
+    if len(sys.argv) > 2:
+      startid = sys.argv[2]
+
+i = 1
+
+q = models.Urls.query.all()
+for u in q:
+  r = epidat_parse.Record()
+  print "Record Number", i
+  print "New Record initiated"
+  print "Now processing ", u.url
+  #FIXME don't add if no hebrew no translation
+  try:
+    #don't forget - some output will be double casue evalRecord gets called twice...
+    r.pEvalRecord(r.getRecord(u.url))
+  except SAXParseException:
+    print "SAXParse exception raised due to crappy xml"
+  else:
+    d = r.pEvalRecord(r.getRecord(u.url))
+    if d == 0:
+      print "Record parsed successfully"
+      s = models.Epidat(loc=r.loc, url=r.url, licence=unicode(str(r.licence)), title=r.title, urld=r.urld, date=unicode(str(r.date)), insc=r.insc, material=r.material, condition=unicode(str(r.condition)), decoration=unicode(r.decoration), geoname=r.geoname, geotype=r.geotype, geocountry=r.geocountry, georegion=r.georegion, geocoord=r.geocoord, images=unicode(str(r.images)), idd=r.idd, sex=unicode(str(r.sex)), pname=unicode(str(r.pname)), deathdate=unicode(str(r.deathdate)), edition=unicode(r.edition), verso=unicode(r.verso), recto=unicode(r.recto), translation=unicode(r.translation), linecomm=unicode(str(r.linecomm)), endcomm=unicode(r.endcomm), proso=unicode(r.proso), bibliography=unicode(r.bibliography))
+      db.session.add(s)
+      try:
+        db.session.commit()
+        print "New record added"
+        i += 1
+      except SAXParseException:
+        print "Error while adding record"
+        if d is False:
+          print "Error while parsing"
--- poerelief/epidat_parse_bfs.py
+++ poerelief/epidat_parse_bfs.py
@@ -6,7 +6,9 @@ sys.setdefaultencoding('utf-8')
 #from io import StringIO, BytesIO
 from loc import loc
 from types import *
+from urllib2 import urlopen
 from bs4 import BeautifulSoup
+#simplejson?
 # Temporär JS code der abfragt  returns one json array  refreshes every so often‘’ or so
 skipped = []
 #Frage an Thomas - Warum <p> tags in xml?? vorgesehen in epidat?
@@ -27,320 +29,80 @@ id = 1
 
 class Record(object):
   def __init__(self):
-    self.id = 0
-    self.loc = ""
-    self.url = ""
-    #self.expr = ""
-    self.licence = ""
-    self.title = ""
-    self.urld = ""
-    self.date = ""
-    self.insc = ""
-    self.material = ""
-    self.condition = ""
-    self.decoration = ""
-    self.geoname = ""
-    self.geotype = ""
-    self.geocountry = ""
-    self.georegion = ""
-    self.geocoord = ""
-    self.images = []
-    self.idd = ""
-    self.sex = ""
-    self.pname = ""
-    self.deathdate = ""
-    self.edition = ""
-    self.verso = ""
-    self.recto = ""
-    self.translation = ""
-    self.linecomm = []
-    self.endcomm = ""
-    self.proso = ""
-    self.bibliography = ""
+    self.data = {}
 
 #This gets a record from supplied id + loc
 
   def getRecord(self, url):
-    self.url = url
-    rec = untangle.parse(url)
-    return rec
+    o = urlopen(url)
+    bfs = BeautifulSoup(bfs)
+    return bfs
 
-  def pEvalRecord(self, record):
-    #availability #licencename #licencelink
-      record.TEI.teiHeader.fileDesc.publicationStmt.availability['status']
-
-      record.TEI.teiHeader.fileDesc.publicationStmt.availability.licence.ref.cdata
-      record.TEI.teiHeader.fileDesc.publicationStmt.availability.licence.ref['target']
-
-      self.licence = [record.TEI.teiHeader.fileDesc.publicationStmt.availability['status'], record.TEI.teiHeader.fileDesc.publicationStmt.availability.licence.ref.cdata, record.TEI.teiHeader.fileDesc.publicationStmt.availability.licence.ref['target']]
-      self.title = record.TEI.teiHeader.fileDesc.titleStmt.title.cdata
+  def pEvalRecord(self, bsf):
+    #availability
+    a = bsf.availability
+    self.availability = a['status']
+    #licence
+      licence = bsf.licence.re
+      #title
+      self.title = bsf.title.text
+    #idno/locid
+    bsf.idno.text
     #urld
-    try:
-      record.TEI.teiHeader.fileDesc.publicationStmt.idno[1].cdata
-    except AttributeError:
-      print "AttributeError was raised by urld"
-      self.urld = None
-    else:
-      self.urld = record.TEI.teiHeader.fileDesc.publicationStmt.idno[1].cdata
+    temp = bsf.find_all('idno')
+    urld = temp[1].text
     #date
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.date
-    except IndexError:
-      "IndexError was raised for date"
-      self.date = None
-    else:
-      self.date = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.date['notBefore']
+    bfs.date.text OR bfs.date['notbefore']
     #insc
-    try:
-      self.insc = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.support.p.cdata
-    except IndexError:
-      print "IndexError was raised for insc"
-      self.insc = None
-    else:
-      self.insc = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.support.p.cdata
+    bfs.support.p OR bfs.support
     #material
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.support.p.material.cdata
-    except IndexError:
-      print "IndexError was raised by material"
-      self.material = None
-    else:
-      self.material = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.support.p.material.cdata
+    bsf.material.text
     #condition
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.condition
-    except IndexError:
-      print "Index Error was raised for condition"
-      self.condition = None
-    else:
-      if type(record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.condition.p) is ListType:
-        self.condition = list(self.condition)
-        for p in record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.condition.p:
-          self.condition.append(p)
-      else:
-        self.condition = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.objectDesc.supportDesc.condition.p.cdata
+    bsf.condition.text
     #Decodescription #Decotype
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.decoDesc.decoNote
-    except IndexError:
-      print "Index Error was raised for decotype"
-      self.decoration = None
-    else:
-      if type(record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.decoDesc.decoNote) is ListType:
-        self.decoration = list(self.decoration)
-        for v in record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.decoDesc.decoNote:
-          self.decoration.append(v.cdata + "," + v['type'])
-      else:
-        self.decoration = [record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.decoDesc.decoNote.cdata, record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.physDesc.decoDesc.decoNote['type']]
+    bfs.deconote, bfs.decodesc
     #Geoname
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.settlement.geogName.cdata
-    except IndexError:
-      print "IndexError was raised by geoname"
-      self.geoname = None
-    else:
-      self.geoname = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.settlement.geogName.cdata
+    eDesc.msDesc.history.origin.settlement.geogName.cdata
     #geotype
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.settlement['type']
-    except IndexError:
-      print "IndexError raised for geotype"
-      self.geotype = None
-    else:
-      self.geotype = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.settlement['type']
+    bfs.settlement.type
     #return type, name
     #geocountry
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.country.cdata
-    except IndexError:
-      print "IndexError was raised by geocountry"
-      self.geocountry = None
-    else:
-      self.geocountry = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.country.cdata
+    bfs.country # wie nur an text, OHNE child tag?
     #georegion
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.country.region.cdata
-    except IndexError:
-      print "IndexError raised for georegion"
-    else:
-      self.georegion = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.country.region.cdata
+    bfs.region.text
     #geocoord
-    try:
-      record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.settlement.geogName.geo.cdata
-    except IndexError:
-      print "IndexError was raised by geocoord"
-      self.geocoord = None
-    else:
-      self.geocoord = record.TEI.teiHeader.fileDesc.sourceDesc.msDesc.history.origin.settlement.geogName.geo.cdata
+    bfs.geo
     # Graphics
+    bfs.graphic bfs.graphic['url'] bfs.graphic.ref['foto1']
+    #das gleiche für foto2, neue tabelle für fotos?
     #FIXME[1] ... check with len or so how many ... url with r.TEI.facsimile.graphic[0]['url'] + add recto verso stuff etc
-    try:
-      record.TEI.facsimile.graphic
-    except IndexError:
-      print "IndexError was raised for graphic"
-      self.images = None
-    else:
-      self.images = record.TEI.facsimile.graphic[0]
+  ### SEparate table for persons, graphics, to know recto vers etc ... maybe also separate for translation etc? ###
     # add stuff about authors, etc r.TEI.teiHeader.encodingDesc.classDecl.taxonomy.category ff
-    try:
-      record.TEI.teiHeader.fileDesc.publicationStmt.idno[0].cdata
-    except AttributeError:
-      print "AttributeError was raised for loc n id"
-      self.idd = None
-    else:
-      self.idd = record.TEI.teiHeader.fileDesc.publicationStmt.idno[0].cdata
+    bfs.idno
     #sex
-    try:
-      record.TEI.teiHeader.profileDesc.particDesc
-    except IndexError:
-      print "IndexError was raised for particDesc + person + sex"
-      self.sex = None
-    else:
-      if type(record.TEI.teiHeader.profileDesc.particDesc.listPerson.person) is ListType:
-        self.sex = list(self.sex)
-        for s in record.TEI.teiHeader.profileDesc.particDesc.listPerson.person:
-          self.sex.append(s['sex'])
-        print "List was added for sex"
-      else:
-        self.sex = record.TEI.teiHeader.profileDesc.particDesc.listPerson.person['sex'] #1 is male
-        print "String was added for sex"
+   bfs.person['sex']
+   #FIXME: für mehrere personen
     #person name
-    try:
-      record.TEI.teiHeader.profileDesc.particDesc
-    except IndexError:
-      print "Index Error was raised for particDesc + person + name"
-      self.pname = None
-    else:
-      if type(record.TEI.teiHeader.profileDesc.particDesc.listPerson.person) is ListType:
-        self.pname = list(self.pname)
-        for p in record.TEI.teiHeader.profileDesc.particDesc.listPerson.person:
-          self.pname.append(p.persName.cdata)
-      else:
-        self.pname = record.TEI.teiHeader.profileDesc.particDesc.listPerson.person.persName.cdata #maybe as list with id if more than one??
+    bfs.person.persname.text
+    #FIXME maybe as list with id if more than one??
     #deathdate
-    try:
-      record.TEI.teiHeader.profileDesc.particDesc.listPerson.person
-    except IndexError:
-      print "IndexError was raised for person"
-    else:
-      if type(record.TEI.teiHeader.profileDesc.particDesc.listPerson.person) is ListType:
-        self.deathdate = list(self.deathdate)
-        for d in record.TEI.teiHeader.profileDesc.particDesc.listPerson.person:
-          try:
-            d.event
-          except IndexError:
-            print "Error was raised during events"
-          else:
-            if d.event['type'] == "dateofdeath":
-              self.deathdate.append(d.event['when'])
-      else:
-        try:
-          record.TEI.teiHeader.profileDesc.particDesc.listPerson.person.event
-        except IndexError:
-          print "IndexError was raised for event"
-          self.deathdate = None
-        else:
-          if record.TEI.teiHeader.profileDesc.particDesc.listPerson.person.event['type'] == "dateofdeath":
-            self.deathdate = record.TEI.teiHeader.profileDesc.particDesc.listPerson.person.event['when']
+    bfs.event['dateofdeath']
     #edition
-    try:
-      record.TEI.text.body.div[0].head.cdata
-    except IndexError:
-      print "IndexError was raised for Edition"
-    else:
-      self.edition = record.TEI.text.body.div[0].head.cdata
+    bsf.find_all("div", type="edition")
     #verso
-    try:
-      record.TEI.text.body.div[0].div
-    except IndexError:
-      print "IndexError was raised for verso"
-      self.verso = None
-    else:
-      if type(record.TEI.text.body.div[0].div[0]) is NoneType:
-        self.verso = None
-      else:
-        self.verso = record.TEI.text.body.div[0].div[0].ab.cdata
+    bsf.find_all("div", subtype="verso")
     #recto
-    try:
-      record.TEI.text.body.div[0].div
-    except IndexError:
-      print "IndexError was raised for recto"
-      self.recto = None
-    else:
-      if type(record.TEI.text.body.div[0].div[1]) is NoneType:
-        self.recto = None
-      else:
-        self.recto = record.TEI.text.body.div[0].div[1].ab.cdata
+    bsf.find_all("div", subtype="recto")
     #translation
-    try:
-      record.TEI.text.body.div[1].div
-    except IndexError:
-      print "IndexError was raised for translation"
-      self.translation = None
-    else:
-      try:
-        record.TEI.text.body.div[1].div.ab
-      except AttributeError:
-        print "IndexError was raised for translation-ab"
-        self.translation = record.TEI.text.body.div[1].div
-      else:
-        self.translation = record.TEI.text.body.div[1].div.ab.cdata
+    bsf.find_all("div", type="translation")
     #linecomm
-    try:
-      record.TEI.text.body.div[2]
-    except IndexError:
-      print "Index Error was raised for linecomm"
-      self.linecomm = None
-    else:
-      try:
-        record.TEI.text.body.div[2].p
-      except IndexError:
-        print "IndexError was raised for linecomm p"
-        self.linecomm = record.TEI.text.body.div[2]
-      else:
-        # head bis ende? cdata anhängen? zzt mit <p> als list
-        for i in record.TEI.text.body.div[2].p:
-          if type(i) != NoneType:
-            self.linecomm.append(i.cdata)
-          else:
-            pass
+    bsf.find_all("div", subtype="Zeilenkommentar")
     #endcomm
-    try:
-      record.TEI.text.body.div[3]
-    except IndexError:
-      print "IndexError was raised for endcomm"
-      self.endcomm = None
-    else:
-      #self.endcomm = record.TEI.text.body.div[3].p
-      try:
-        record.TEI.text.body.div[3].p
-      except IndexError:
-        print "IndexError was raised for part of endcomm"
-        self.endcomm = record.TEI.text.body.div
-      else:
-        self.endcomm = record.TEI.text.body.div[3].p
+    bsf.find_all("div", subtype="Endkommentar")
     #proso
-    try:
-      record.TEI.text.body.div[4]
-    except IndexError:
-      print "IndexError was raised for proso"
-      self.proso = None
-    else:
-      try:
-        record.TEI.text.body.div[4].p
-      except IndexError:
-        print "IndexError, no p in proso"
-        self.proso = None
-      else:
-        self.proso = record.TEI.text.body.div[4].p
+    bsf.find_all("div", subtype="Prosopographie")
     #bibliography
-    try:
-      record.TEI.text.body.div[5]
-    except IndexError:
-      print "IndexError was raised for bibliography"
-      self.bibliography = None
-    else:
-      self.bibliography = record.TEI.text.body.div[5] # if list, for p in l ... cdata
+    bsf.find_all("div", type="bibliography")
     return 0
 
 # format data as json
