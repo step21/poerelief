37053ebf8c52f318c2a3a064b2d670f4216c70fc
FI <step21@devtal.de>
2014-07-02 05:39:22 +0200

    more parsing changes, now using beautifulsoup and a dictionary

--- poerelief/epidat_parse_bfs.py
+++ poerelief/epidat_parse_bfs.py
@@ -29,80 +29,82 @@ id = 1
 
 class Record(object):
   def __init__(self):
-    self.data = {}
+    self.data = {'availability': "", 'licence': "", 'title': "",'locid': "",'urld': "",'date': "",'insc': "",'material': "", 'condition': '', 'deconote': '', 'decodesc': '', 'geoname': '', 'geotype': '', 'geocountry': '', 'georegion': '', 'geocoord': '', 'graphics': '', 'graphicsurl': '', 'idno': '', 'sex': '', 'pname': '', 'deathdate': '', 'edition': '', 'verso': '', 'recto': '', 'translation': '', 'linecomm':  '', 'endcomm': '', 'proso': '', 'bibliography',: ''}
 
 #This gets a record from supplied id + loc
 
   def getRecord(self, url):
     o = urlopen(url)
-    bfs = BeautifulSoup(bfs)
-    return bfs
+    f = BeautifulSoup(bfs)
+    return f
 
   def pEvalRecord(self, bfs):
     #availability
     a = bsf.availability
-    self.availability = a['status']
+    self.data.update('availability': a['status'])
     #licence
-    self.licence = bsf.licence.re
+    self.data.update('licence': bfs.licence.re)
       #title
-    self.self.title = bsf.title.text
+    self.data.update('title': bfs.title.text)
     #idno/locid
-    self = bsf.idno.text
+    self.data.update('locid': bfs.idno.text)
     #urld
     temp = bsf.find_all('idno')
-    self.urld = temp[1].text
+    self.data.update('urld': temp[1].text)
     #date
-    self = bfs.date.text #OR bfs.date['notbefore']
+    self.data.update('date': bfs.date.text) #OR bfs.date['notbefore']
     #insc
-    self = bfs.support.p #OR bfs.support
+    self.data.update('insc': bfs.support.p) #OR bfs.support
     #material
-    self = bsf.material.text
+    self.data.update('material': bfs.material.text)
     #condition
-    self = bsf.condition.text
+    self.data.update('condition': bfs.condition.text)
     #Decodescription #Decotype
-    self = bfs.deconote, bfs.decodesc
+    self.data.update('deconote': bfs.deconote)
+    self.data.update('decodesc': bfs.decodesc)
     #Geoname
-    self = eDesc.msDesc.history.origin.settlement.geogName.cdata
+    self.data.update('geoname': bfs.geogname.find(text=True, recursive=False))
     #geotype
-    self = bfs.settlement.type
+    self.data.update('geotype': bfs.settlement.type)
     #return type, name
     #geocountry
-    self = bfs.country # wie nur an text, OHNE child tag?
+    self.data.update('geocountry': bfs.country.find(text=True, recursive=False)) # wie nur an text, OHNE child tag?
     #georegion
-    self = bfs.region.text
+    self.data.update('georegion': bfs.region.text)
     #geocoord
-    self = bfs.geo
+    self.data.update('geocoord': bfs.geo)
     # Graphics
-    self.graphics = bfs.graphic bfs.graphic['url'] bfs.graphic.ref['foto1']
+    self.data.update('graphics': bfs.graphic)
+    self.data.update('graphicsurl': bfs.graphic['url']) #bfs.graphic.ref['foto1']
     #das gleiche für foto2, neue tabelle für fotos?
     #FIXME[1] ... check with len or so how many ... url with r.TEI.facsimile.graphic[0]['url'] + add recto verso stuff etc
   ### SEparate table for persons, graphics, to know recto vers etc ... maybe also separate for translation etc? ###
     # add stuff about authors, etc r.TEI.teiHeader.encodingDesc.classDecl.taxonomy.category ff
-    bfs.idno
+    self.data.update('idno': bfs.idno)
     #sex
-    bfs.person['sex']
+    self.data.update('sex': bfs.person['sex'])
     #FIXME: für mehrere personen
     #person name
-    bfs.person.persname.text
+    self.data.update('pname': bfs.person.persname.text)
     #FIXME maybe as list with id if more than one??
     #deathdate
-    bfs.event['dateofdeath']
+    self.data.update('deathdate': bfs.event['dateofdeath'])
     #edition
-    bsf.find_all("div", type="edition")
+    self.data.update('edition': bfs.find_all("div", type="edition"))
+     #recto
+    self.data.update('recto': bfs.find_all("div", subtype="recto"))
     #verso
-    bsf.find_all("div", subtype="verso")
-    #recto
-    bsf.find_all("div", subtype="recto")
+    self.data.update('verso': bfs.find_all("div", subtype="verso"))
     #translation
-    bsf.find_all("div", type="translation")
+    self.data.update('translation': bfs.find_all("div", type="translation"))
     #linecomm
-    bsf.find_all("div", subtype="Zeilenkommentar")
+    self.data.update('linecomm': bfs.find_all("div", subtype="Zeilenkommentar")
     #endcomm
-    bsf.find_all("div", subtype="Endkommentar")
+    self.data.update('endcomm': bff.find_all("div", subtype="Endkommentar")
     #proso
-    bsf.find_all("div", subtype="Prosopographie")
+    self.data.update('proso': bfs.find_all("div", subtype="Prosopographie")
     #bibliography
-    bsf.find_all("div", type="bibliography")
+    self.data.update('bibliography': bfs.find_all("div", type="bibliography")
     return 0
 
 # format data as json
