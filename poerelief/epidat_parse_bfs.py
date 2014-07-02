60e19b2ee286f9db9cf1637f54276b50e3e12e94
FI <step21@devtal.de>
2014-07-02 15:15:05 +0200

    missing comma

--- poerelief/epidat_parse_bfs.py
+++ poerelief/epidat_parse_bfs.py
@@ -43,68 +43,68 @@ class Record(object):
     a = bsf.availability
     self.data.update('availability', a['status'])
     #licence
-    self.data.update('licence': bfs.licence.re)
+    self.data.update('licence', bfs.licence.re)
       #title
-    self.data.update('title': bfs.title.text)
+    self.data.update('title', bfs.title.text)
     #idno/locid
-    self.data.update('locid': bfs.idno.text)
+    self.data.update('locid', bfs.idno.text)
     #urld
     temp = bsf.find_all('idno')
-    self.data.update('urld': temp[1].text)
+    self.data.update('urld', temp[1].text)
     #date
-    self.data.update('date': bfs.date.text) #OR bfs.date['notbefore']
+    self.data.update('date', bfs.date.text) #OR bfs.date['notbefore']
     #insc
-    self.data.update('insc': bfs.support.p) #OR bfs.support
+    self.data.update('insc', bfs.support.p) #OR bfs.support
     #material
-    self.data.update('material': bfs.material.text)
+    self.data.update('material', bfs.material.text)
     #condition
-    self.data.update('condition': bfs.condition.text)
+    self.data.update('condition', bfs.condition.text)
     #Decodescription #Decotype
-    self.data.update('deconote': bfs.deconote)
-    self.data.update('decodesc': bfs.decodesc)
+    self.data.update('deconote', bfs.deconote)
+    self.data.update('decodesc', bfs.decodesc)
     #Geoname
-    self.data.update('geoname': bfs.geogname.find(text=True, recursive=False))
+    self.data.update('geoname', bfs.geogname.find(text=True, recursive=False))
     #geotype
-    self.data.update('geotype': bfs.settlement.type)
+    self.data.update('geotype', bfs.settlement.type)
     #return type, name
     #geocountry
-    self.data.update('geocountry': bfs.country.find(text=True, recursive=False)) # wie nur an text, OHNE child tag?
+    self.data.update('geocountry', bfs.country.find(text=True, recursive=False)) # wie nur an text, OHNE child tag?
     #georegion
-    self.data.update('georegion': bfs.region.text)
+    self.data.update('georegion', bfs.region.text)
     #geocoord
-    self.data.update('geocoord': bfs.geo)
+    self.data.update('geocoord', bfs.geo)
     # Graphics
-    self.data.update('graphics': bfs.graphic)
-    self.data.update('graphicsurl': bfs.graphic['url']) #bfs.graphic.ref['foto1']
+    self.data.update('graphics', bfs.graphic)
+    self.data.update('graphicsurl', bfs.graphic['url']) #bfs.graphic.ref['foto1']
     #das gleiche für foto2, neue tabelle für fotos?
     #FIXME[1] ... check with len or so how many ... url with r.TEI.facsimile.graphic[0]['url'] + add recto verso stuff etc
   ### SEparate table for persons, graphics, to know recto vers etc ... maybe also separate for translation etc? ###
     # add stuff about authors, etc r.TEI.teiHeader.encodingDesc.classDecl.taxonomy.category ff
-    self.data.update('idno': bfs.idno)
+    self.data.update('idno', bfs.idno)
     #sex
-    self.data.update('sex': bfs.person['sex'])
+    self.data.update('sex', bfs.person['sex'])
     #FIXME: für mehrere personen
     #person name
-    self.data.update('pname': bfs.person.persname.text)
+    self.data.update('pname', bfs.person.persname.text)
     #FIXME maybe as list with id if more than one??
     #deathdate
-    self.data.update('deathdate': bfs.event['dateofdeath'])
+    self.data.update('deathdate', bfs.event['dateofdeath'])
     #edition
-    self.data.update('edition': bfs.find_all("div", type="edition"))
+    self.data.update('edition', bfs.find_all("div", type="edition"))
      #recto
-    self.data.update('recto': bfs.find_all("div", subtype="recto"))
+    self.data.update('recto', bfs.find_all("div", subtype="recto"))
     #verso
-    self.data.update('verso': bfs.find_all("div", subtype="verso"))
+    self.data.update('verso', bfs.find_all("div", subtype="verso"))
     #translation
-    self.data.update('translation': bfs.find_all("div", type="translation"))
+    self.data.update('translation', bfs.find_all("div", type="translation"))
     #linecomm
-    self.data.update('linecomm': bfs.find_all("div", subtype="Zeilenkommentar")
+    self.data.update('linecomm', bfs.find_all("div", subtype="Zeilenkommentar")
     #endcomm
-    self.data.update('endcomm': bff.find_all("div", subtype="Endkommentar")
+    self.data.update('endcomm', bff.find_all("div", subtype="Endkommentar")
     #proso
-    self.data.update('proso': bfs.find_all("div", subtype="Prosopographie")
+    self.data.update('proso', bfs.find_all("div", subtype="Prosopographie")
     #bibliography
-    self.data.update('bibliography': bfs.find_all("div", type="bibliography")
+    self.data.update('bibliography', bfs.find_all("div", type="bibliography")
     return 0
 
 # format data as json
