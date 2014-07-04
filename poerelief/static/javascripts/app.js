c20fed341afe795a6ac03f8d9a017fe67599a14c
FI <step21@devtal.de>
2014-07-04 02:54:26 +0200

    ajax loading implemented, though doesn't work as it should just yet ...

--- poerelief/static/javascripts/app.js
+++ poerelief/static/javascripts/app.js
@@ -3,8 +3,76 @@ $( document ).ready(function() {
     e.preventDefault();
     var current = $("a.active");
     current.removeClass("active");
+    /* Maybe change such as here for animation? http://jqueryui.com/removeClass/ */
     $(current.data("target")).hide();
     $(this).addClass("active");
     $($(this).data("target")).show();
   });
 });
+/* var S = require('string'); */
+$(document).ready(function() {
+
+	$.getJSON('/doc/random', function( data ) {
+		/* Name, date and pictures work quite well, though sometimes I have seen that there is no name and pictures are very different from each other */
+		/* The rest of the text however is a problem ... translation works best  (except it needs whitespace stripped, or something ... I alread strip ab/lb tags that were still in the source date, but it's not enough)
+		 hebrew is more of a problem, as it is not always pure hebrew ... according to Thomas from the institute, recto/verso (as the original is called in the source)
+		  should almost always be hebrew. That is not always true though, sometimes it also is mixed with german ...  (and either one can be empty) */
+			/* Also, if a a specific record is requested, that should be injected instead of a random one ... though not sure how to pass that through atm. In the template it is {{ret}} but not sure how to inser that into javascript */		  
+			var translation = S(data.translation).stripTags().trim();
+			$('#translation').text(translation);
+			$('#original').html(data.verso + data.recto);
+			/*$('#original').append();*/
+			$('.center-cropped').css('background-image', "url("+data.graphicsurl+")");
+			$('#pname').html(data.pname + " [" + data.date + "]");
+			});
+
+		});
+/*$(document).ready(function() { */
+	/* This replaces the url path (in modern browsers) with a permalink - just have to get rid of <idno></idno> tags */
+	/*var idno = S(data.idno).stripTags().s;
+	var permalink = "/doc/random/" + $.idno.text();
+	window.history.replaceState("": ,"", permalink);
+}); */
+/*
+<div class="center-cropped" 
+       style="background-image: url('http://www.steinheim-institut.de/daten/picsdu4/xl/0002_du4_2009.png');">
+  </div>​
+</div>
+<div>
+  <div class="text-navigation">
+    <a href='#' data-target="#original_and_translation">Side to Side</a>
+    <a href='#' data-target="#translation">Translation</a>
+    <a href='#' data-target="#original" class="active">Original</a>
+  </div>
+  <div class="clear"></div>
+  <div id="original" class="text hebrew">
+פ״נ‏‎
+‎‏איש תם וישר צעיר לימים‏‎
+‎‏כמ״ר יוסף בר צבי נפטר כ״א‏‎
+‎‏לחודש מנחם אב שנת תר״ץ‏‎
+‎‏תנצב״ה‏‎
+  </div>
+  <div id="translation" class="text">
+Hier ist begraben
+ein lauterer und aufrechter Mann, jung an Tagen,
+der geehrte Herr Josef, Sohn des Zwi, verschieden 21.
+des Monats Menachem Aw des Jahres 690.
+Seine Seele sei eingebunden in das Bündel des Lebens
+  </div>
+  <div id="original_and_translation">
+    <div class="text hebrew left">
+פ״נ‏‎
+‎‏איש תם וישר צעיר לימים‏‎
+‎‏כמ״ר יוסף בר צבי נפטר כ״א‏‎
+‎‏לחודש מנחם אב שנת תר״ץ‏‎
+‎‏תנצב״ה‏‎
+    </div>
+    <div class="text right">
+Hier ist begraben
+ein lauterer und aufrechter Mann, jung an Tagen,
+der geehrte Herr Josef, Sohn des Zwi, verschieden 21.
+des Monats Menachem Aw des Jahres 690.
+Seine Seele sei eingebunden in das Bündel des Lebens
+    </div>
+    <div class="clear"></div>
+  </div>*/
--- poerelief/views.py
+++ poerelief/views.py
@@ -16,12 +16,13 @@ def page():
 	pg = ""
 	return render_template("index.html", pg=pg, sitename=sitenamed, pagetitle=pagetitle, version=version)
 
+#This also gets a specific dataset ... not sure which is better ...
 @app.route('/doc/<locid>')
 def epidoc_json(locid):
 	doc = models.Epidat.query.filter_by(locid='locid').first_or_404()
 	data = {'availability': doc.availability, 'licence': doc.licence, 'title': doc.title, 'locid': doc.locid, 'urld': doc.urld, 'date': doc.date, 'insc': doc.insc, 'material': doc.material, 'condition': doc.condition, 'deconote': doc.deconote, 'decodesc': doc.decodesc, 'geoname': doc.geoname, 'geotype': doc.geotype, 'geocountry': doc.geocountry, 'georegion': doc.georegion, 'geocoord': doc.geocoord, 'graphics': doc.graphics, 'graphicsurl': doc.graphicsurl, 'idno': doc.idno, 'sex': doc.sex, 'pname': doc.pname, 'deathdate': doc.deathdate, 'edition': doc.edition, 'verso': doc.verso, 'recto': doc.recto, 'translation': doc.translation, 'linecomm':  doc.linecomm, 'endcomm': doc.endcomm, 'proso': doc.proso, 'bibliography': doc.bibliography}
 
-	return json.dump(data)
+	return json.dumps(data)
 
   #return render_template("index.html", pg=s.id + s.loc s.translation)
 
@@ -29,7 +30,7 @@ def epidoc_json(locid):
 
 #javascript to switch texts
 
-#implement random json array?
+#this gets a random dataset
 @app.route('/doc/random')
 def randomdoc():
 	rand = random.randrange(0, db.session.query(models.Epidat).count()) 
@@ -37,10 +38,24 @@ def randomdoc():
 	data = {'availability': doc.availability, 'licence': doc.licence, 'title': doc.title, 'locid': doc.locid, 'urld': doc.urld, 'date': doc.date, 'insc': doc.insc, 'material': doc.material, 'condition': doc.condition, 'deconote': doc.deconote, 'decodesc': doc.decodesc, 'geoname': doc.geoname, 'geotype': doc.geotype, 'geocountry': doc.geocountry, 'georegion': doc.georegion, 'geocoord': doc.geocoord, 'graphics': doc.graphics, 'graphicsurl': doc.graphicsurl, 'idno': doc.idno, 'sex': doc.sex, 'pname': doc.pname, 'deathdate': doc.deathdate, 'edition': doc.edition, 'verso': doc.verso, 'recto': doc.recto, 'translation': doc.translation, 'linecomm':  doc.linecomm, 'endcomm': doc.endcomm, 'proso': doc.proso, 'bibliography': doc.bibliography}
 	return json.dumps(data)
 
-@app.route('/demo')
-def demo():
-	ret = 'var jsonLikeString = "name:red, type:blue, multiples:green, cat:brown"'
-	return ret
+#this generates a specific json
+@app.route('/doc/random/<idno>')
+def permajson(idno):
+	if idno:
+		doc = models.Epidat.query.filter_by(locid=idno).first()
+		data = {'availability': doc.availability, 'licence': doc.licence, 'title': doc.title, 'locid': doc.locid, 'urld': doc.urld, 'date': doc.date, 'insc': doc.insc, 'material': doc.material, 'condition': doc.condition, 'deconote': doc.deconote, 'decodesc': doc.decodesc, 'geoname': doc.geoname, 'geotype': doc.geotype, 'geocountry': doc.geocountry, 'georegion': doc.georegion, 'geocoord': doc.geocoord, 'graphics': doc.graphics, 'graphicsurl': doc.graphicsurl, 'idno': doc.idno, 'sex': doc.sex, 'pname': doc.pname, 'deathdate': doc.deathdate, 'edition': doc.edition, 'verso': doc.verso, 'recto': doc.recto, 'translation': doc.translation, 'linecomm':  doc.linecomm, 'endcomm': doc.endcomm, 'proso': doc.proso, 'bibliography': doc.bibliography}
+	else:
+		data = "No valid id-loc specified."
+	return json.dumps(data)
+
+#This get a specific record.
+@app.route('/<idno>')
+def permalink(idno):
+	if idno:
+		ret = idno
+	else:
+		ret = "no valid id-loc specified"
+	return render_template("index.html", ret=ret, sitename=sitenamed, pagetitle=pagetitle, version=version)
 
 	"""If you write a Flask view function it’s often very handy to return a 404 error for missing entries. Because this is a very common idiom, Flask-SQLAlchemy provides a helper for this exact purpose. Instead of get() one can use get_or_404() and instead of first() first_or_404(). This will raise 404 errors instead of returning None:
 
