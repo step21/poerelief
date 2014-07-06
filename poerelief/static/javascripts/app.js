5968d0d82c4d666b38ab7b3f42186cbd6572f747
FI <step21@devtal.de>
2014-07-06 02:53:49 +0200

    last changes?

--- poerelief/static/javascripts/app.js
+++ poerelief/static/javascripts/app.js
@@ -26,50 +26,8 @@ function getDoc(docid) {
 		/*$('#original').append();*/
 		$('.center-cropped').css('background-image', "url("+data.graphicsurl+")");
 		$('#pname').html(data.pname + " [" + data.date + "]");
+    /* window.history.pushState({}, "Poetic Relief", "/" + data.locid); */
     window.history.pushState({}, "Poetic Relief", "/poerelief/" + data.locid);
 	});
 
 };
-/*
-<div class="center-cropped" 
-       style="background-image: url('http://www.steinheim-institut.de/daten/picsdu4/xl/0002_du4_2009.png');">
-  </div>​
-</div>
-<div>
-  <div class="text-navigation">
-    <a href='#' data-target="#original_and_translation">Side to Side</a>
-    <a href='#' data-target="#translation">Translation</a>
-    <a href='#' data-target="#original" class="active">Original</a>
-  </div>
-  <div class="clear"></div>
-  <div id="original" class="text hebrew">
-פ״נ‏‎
-‎‏איש תם וישר צעיר לימים‏‎
-‎‏כמ״ר יוסף בר צבי נפטר כ״א‏‎
-‎‏לחודש מנחם אב שנת תר״ץ‏‎
-‎‏תנצב״ה‏‎
-  </div>
-  <div id="translation" class="text">
-Hier ist begraben
-ein lauterer und aufrechter Mann, jung an Tagen,
-der geehrte Herr Josef, Sohn des Zwi, verschieden 21.
-des Monats Menachem Aw des Jahres 690.
-Seine Seele sei eingebunden in das Bündel des Lebens
-  </div>
-  <div id="original_and_translation">
-    <div class="text hebrew left">
-פ״נ‏‎
-‎‏איש תם וישר צעיר לימים‏‎
-‎‏כמ״ר יוסף בר צבי נפטר כ״א‏‎
-‎‏לחודש מנחם אב שנת תר״ץ‏‎
-‎‏תנצב״ה‏‎
-    </div>
-    <div class="text right">
-Hier ist begraben
-ein lauterer und aufrechter Mann, jung an Tagen,
-der geehrte Herr Josef, Sohn des Zwi, verschieden 21.
-des Monats Menachem Aw des Jahres 690.
-Seine Seele sei eingebunden in das Bündel des Lebens
-    </div>
-    <div class="clear"></div>
-  </div>*/
