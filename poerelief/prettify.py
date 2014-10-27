981ea9908a4a1937fa5330c1047c73d6414f1520
FI <step21@devtal.de>
2014-10-27 12:40:53 +0100

    prettify mod and layout changes

---
+++ poerelief/prettify.py
@@ -0,0 +1,22 @@
+class Prettify(self):
+  def __init__(self):
+    
+def stripHTMLTags (html):
+  """
+    Strip HTML tags from any string and transfrom special entities
+  """
+  import re
+  text = html
+ 
+  # apply rules in given order!
+  rules = [
+    { r'\s+' : u' '},                   # replace consecutive spaces
+    { r'^\s+' : u'' }                   # remove spaces at the beginning
+  ]
+ 
+  for rule in rules:
+    for (k,v) in rule.items():
+      regex = re.compile (k)
+      text  = regex.sub (v, text)
+ 
+  return text
