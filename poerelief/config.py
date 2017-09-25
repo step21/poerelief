763f1df3cb3f9bba94a12baf942a33ad449b8950
step21 <step21@devtal.de>
2017-09-25 09:34:41 +0200

    initial try to detect hostname

--- poerelief/config.py
+++ poerelief/config.py
@@ -1,3 +1,8 @@
 # encoding=utf-8
+from flask import request
+
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = "sqlite:///teidb_dev.sqlite"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+# "outside of request context :-/"
+SERVER_NAME = urlparse(request.host)
