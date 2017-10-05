b37e1fbf3643232c1874d00f2d72056e996a87da
step21 <step21@devtal.de>
2017-10-05 02:08:27 +0200

    clean up in files and structure

--- poerelief/__init__.py
+++ poerelief/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
