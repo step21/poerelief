2f2e91982eda0addc4baec2a221cd8d91e5b5f16
step21 <step21@devtal.de>
2017-10-05 03:48:28 +0200

    cleanup for new data collection

---
+++ harvest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding=utf-8
+import sys
+sys.path.append("poerelief/")
+from data_harvesting import *
+from poerelief import app
+app.run(debug = True)
---
+++ poerelief/data_harvesting/harvester.py
@@ -0,0 +1,15 @@
+#data_harvester
+import sys
+
+class Harvester():
+
+	def getLocations():
+		return loc = ["ad2", "aha", "alt", "ans", "ar1", "bme", "arl", "bay", "bee", "bng", "blr", "sb3", "bom", "bns", "bor", "hrs", "wal", "e51", "e01", "e99", "e02", "brh", "dds", "e03", "e04", "du2", "du3", "du4", "du1", "du6", "du5", "erf", "wsw", "kuc", "ffb", "ffs", "ffh", "e05", "e52", "gsk", "gws", "e10", "e11", "e12", "e13", "e14", "hgl", "hgw", "hbs", "hha", "hht", "hmr", "hdh", "hnn", "hns", "blu", "hen", "hld", "hos", "hxf", "ils", "imr", "e06", "e17", "e18", "e19", "kal", "e20", "kob", "e21", "e22", "kre", "e23", "e42", "e43", "lau", "lin", "mz2", "e25", "sb1", "msn", "e26", "e31", "e30", "e29", "e27", "e28", "mue", "e32", "e41", "sb5", "e33", "obw", "ora", "sb0", "pro", "reg", "rex", "bbr", "e54", "e53", "e34", "e35", "rth", "oes", "sbs", "sb2", "sb9", "sb6", "srm", "e09", "e16", "smk", "e36", "sgb", "e58", "sdh", "sts", "spy", "sb7", "sb8", "sb4", "roe", "e37", "e44", "e60", "e61", "e38", "e39", "e40", "lom", "ver", "e07", "e08", "win", "wrm", "hdf"]
+
+	def loadData():
+		pass
+
+	def updateData():
+		pass
+
+
